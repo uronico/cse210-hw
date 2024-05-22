56056cd997d814e5c3209ad934e5e139c4720a29
uronico <uronico3@gmail.com>
2024-05-23 03:04:53 +0800

    create a class

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base (color)
+    {
+        _radius = radius;
+    }
+
+    // Notice the use of the override keyword here
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
