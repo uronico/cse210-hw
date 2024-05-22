bee64d9fabb5198676f507551303a2b351079382
uronico <uronico3@gmail.com>
2024-05-23 03:06:15 +0800

    create a program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -19,11 +19,9 @@ class Program
 
         foreach (Shape s in shapes)
         {
-            // Notice that all shapes have a GetColor method from the base class
+        
             string color = s.GetColor();
 
-            // Notice that all shapes have a GetArea method, but the behavior is
-            // different for each type of shape
             double area = s.GetArea();
 
             Console.WriteLine($"The {color} shape has an area of {area}.");
