4806c70ec4c3109865e120077d2b71d2d9de8b9c
uronico <uronico3@gmail.com>
2024-06-04 04:09:50 +0800

    create a program

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation3 World!");
+
+        Address address = new Address("123 Main St", "Anytown","CA","12345");
+
+        Lecture lecture = new Lecture("Introduction to AI", "Learn about AI from a world-class expert.", new DateTime(2023, 03, 20), new TimeSpan(14, 0, 0), address, "Dr. Jane Smith", 100 );
+        Reception reception = new Reception("Networking Event", "Join us for an evening of networking and refreshments.", new DateTime(2023, 03, 20), new TimeSpan(18, 0, 0), address, "nicouro@company.com");
+        OutdoorGathering gathering = new OutdoorGathering("End of Summer BBQ", "Join us for our end of summer BBQ.", new DateTime(2023, 03, 20), new TimeSpan(16, 0, 0), address, "Partly cloudy, high of 70°F, low of 50°F.");
+
+        Console.WriteLine(lecture.GetFullDetails());
+        Console.WriteLine("");
+        Console.WriteLine(reception.GetFullDetails());
+        Console.WriteLine("");
+        Console.WriteLine(gathering.GetFullDetails());
+
     }
 }
