30f9ac5aad3bf1e337f53bbcd48ce36aa4eef12c
uronico <uronico3@gmail.com>
2024-06-04 04:20:10 +0800

    create a program

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2024, 06, 3), 30, 4.8),
+            new Cycling(new DateTime(2024, 06, 3), 30, 20),
+            new Swimming(new DateTime(2024, 06, 3), 30, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+
     }
 }
